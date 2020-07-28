528c8df0a051c33f4cb43fdde51baa9c96f05638
arihant-2310 <arihant2310@gmail.com>
2020-07-28 20:59:37 +0530

    initial setup

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const MainScreen = () => {
+  return <Text>Welcome To Main Screen</Text>;
+};
+
+export default MainScreen;
