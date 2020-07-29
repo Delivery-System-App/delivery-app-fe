6a0deadae1cf9817aafb22bb4cd666260ba7f84b
Abhiram R Pai <abhirampai1999@gmail.com>
2020-07-29 18:37:46 +0530

    added getid for testing restaurantlisting

--- src/components/RestaurantItem/index.js
+++ src/components/RestaurantItem/index.js
@@ -1,25 +1,29 @@
 import React from "react";
-import {View,Text,ImageBackground,Image} from "react-native";
+import {View,Text,ImageBackground,Image, Button} from "react-native";
 export default function RestaurantItem({restaurant}){
     console.log(restaurant);
 
     if (!restaurant) {
       return null;
     }
-  
+    const returnId=()=>{
+      console.log(restaurant.id)
+    }
     return (
-      <View>
+      <View 
+      key={restaurant.id}
+      >
         <Image
           style={{width: 400, height: 400}}
           source={{ uri: restaurant.image.url }}
-
+          
         ></Image>
-          <Text styleName="md-gutter-bottom">{restaurant.name}
+          <Text>{restaurant.name}
           </Text>
           <Text>
               {restaurant.address}
           </Text>
-        
+        <Button title={"GetId"} onPress={returnId}></Button>
       </View>
     );
   }
