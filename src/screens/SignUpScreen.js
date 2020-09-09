b68a49170133792c9631f710ec26e32752b7c82a
Arihant Hirawat <arihant2310@gmail.com>
2020-09-09 14:34:28 +0530

    adds  scroll view to the signup screen

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { View, StyleSheet, Button, Text, TextInput } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { TouchableOpacity, ScrollView } from "react-native-gesture-handler";
 import { Context as AuthContext } from "./../context/AuthContext";
 import { register } from "../redux/actions";
 import { useDispatch } from "react-redux";
@@ -37,112 +37,116 @@ const SignUp = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 25, marginTop: 20 }}>Welcome back!</Text>
-      <Text style={{ fontSize: 16, color: "gray", marginTop: 20 }}>
-        Sign in to continue
-      </Text>
-      <Animatable.View>
-        <TextInput
-          style={{
-            marginTop: 40,
-            borderBottomColor: "#ddd",
-            borderBottomWidth: 1,
-            paddingBottom: 20,
-          }}
-          value={name}
-          onChangeText={setName}
-          autoCapitalize="none"
-          placeholder="Name"
-          autoCorrect={false}
-        />
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={{ fontSize: 25, marginTop: 20 }}>Welcome back!</Text>
+        <Text style={{ fontSize: 16, color: "gray", marginTop: 20 }}>
+          Sign in to continue
+        </Text>
+        <Animatable.View>
+          <TextInput
+            style={{
+              marginTop: 40,
+              borderBottomColor: "#ddd",
+              borderBottomWidth: 1,
+              paddingBottom: 20,
+            }}
+            value={name}
+            onChangeText={setName}
+            autoCapitalize="none"
+            placeholder="Name"
+            autoCorrect={false}
+          />
 
-        <TextInput
-          style={{
-            marginTop: 40,
-            borderBottomColor: "#ddd",
-            borderBottomWidth: 1,
-            paddingBottom: 20,
-          }}
-          placeholder="Email"
-          value={email}
-          type="email"
-          onChangeText={setEmail}
-          autoCapitalize="none"
-          autoCorrect={false}
-        />
-        <TextInput
-          style={{
-            marginTop: 40,
-            borderBottomColor: "#ddd",
-            borderBottomWidth: 1,
-            paddingBottom: 20,
-          }}
-          placeholder="password"
-          value={password}
-          type="password"
-          onChangeText={setPassword}
-          autoCapitalize="none"
-          autoCorrect={false}
-          secureTextEntry={true}
-        />
-        <TextInput
-          style={{
-            marginTop: 40,
-            borderBottomColor: "#ddd",
-            borderBottomWidth: 1,
-            paddingBottom: 20,
-          }}
-          placeholder="confirm password"
-          value={confirm}
-          type="password"
-          onChangeText={setConfirm}
-          autoCapitalize="none"
-          autoCorrect={false}
-          secureTextEntry={true}
-        />
+          <TextInput
+            style={{
+              marginTop: 40,
+              borderBottomColor: "#ddd",
+              borderBottomWidth: 1,
+              paddingBottom: 20,
+            }}
+            placeholder="Email"
+            value={email}
+            type="email"
+            onChangeText={setEmail}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+          <TextInput
+            style={{
+              marginTop: 40,
+              borderBottomColor: "#ddd",
+              borderBottomWidth: 1,
+              paddingBottom: 20,
+            }}
+            placeholder="password"
+            value={password}
+            type="password"
+            onChangeText={setPassword}
+            autoCapitalize="none"
+            autoCorrect={false}
+            secureTextEntry={true}
+          />
+          <TextInput
+            style={{
+              marginTop: 40,
+              borderBottomColor: "#ddd",
+              borderBottomWidth: 1,
+              paddingBottom: 20,
+            }}
+            placeholder="confirm password"
+            value={confirm}
+            type="password"
+            onChangeText={setConfirm}
+            autoCapitalize="none"
+            autoCorrect={false}
+            secureTextEntry={true}
+          />
 
-        {state.errorMessage ? (
-          <Text style={{ color: "red", textAlign: "center", marginTop: 10 }}>
-            {state.errorMessage}
-          </Text>
-        ) : null}
-      </Animatable.View>
-      <View
-        style={{
-          alignItems: "center",
-          justifyContent: "center",
-          marginTop: 40,
-        }}
-      >
-        <TouchableOpacity
+          {state.errorMessage ? (
+            <Text style={{ color: "red", textAlign: "center", marginTop: 10 }}>
+              {state.errorMessage}
+            </Text>
+          ) : null}
+        </Animatable.View>
+        <View
           style={{
-            width: 200,
-            backgroundColor: "#0d47a1",
-            padding: 10,
             alignItems: "center",
             justifyContent: "center",
-            borderRadius: 40,
-            marginTop: 30,
+            marginTop: 40,
           }}
-          onPress={handleSubmit}
         >
-          {false ? (
-            <Loader />
-          ) : (
-            <Text style={{ textAlign: "center", color: "#FFF", fontSize: 16 }}>
-              Sign up
-            </Text>
-          )}
-        </TouchableOpacity>
-        <View style={{ flexDirection: "row", marginTop: 40 }}>
-          <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
-            <Text style={{ color: "gray" }}>Have an account?</Text>
-            <Text style={{ fontWeight: "bold" }}>Sign in</Text>
+          <TouchableOpacity
+            style={{
+              width: 200,
+              backgroundColor: "#0d47a1",
+              padding: 10,
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 40,
+              marginTop: 30,
+            }}
+            onPress={handleSubmit}
+          >
+            {false ? (
+              <Loader />
+            ) : (
+              <Text
+                style={{ textAlign: "center", color: "#FFF", fontSize: 16 }}
+              >
+                Sign up
+              </Text>
+            )}
           </TouchableOpacity>
+          <View style={{ flexDirection: "row", marginTop: 40 }}>
+            <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
+              <Text style={{ color: "gray" }}>Have an account?</Text>
+              <Text style={{ fontWeight: "bold" }}>Sign in</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
