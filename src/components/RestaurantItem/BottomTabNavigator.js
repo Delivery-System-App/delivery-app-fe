999891f4c14cba8716e0ac846dd1eb62368d77fd
Arihant Hirawat <arihant2310@gmail.com>
2020-09-09 12:47:56 +0530

    adds icon to the bottom tab navigator

---
+++ src/components/RestaurantItem/BottomTabNavigator.js
@@ -0,0 +1,51 @@
+import React from "react";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import ListHotels from "../../screens/ListHotels";
+import Cart from "../../screens/Cart";
+import ProfileScreen from "../../screens/ProfileScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: "#e91e63",
+      }}
+    >
+      <Tab.Screen
+        name="Hotels"
+        component={ListHotels}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={Cart}
+        options={{
+          tabBarLabel: "Cart",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="cart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="UserProfile"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigator;
