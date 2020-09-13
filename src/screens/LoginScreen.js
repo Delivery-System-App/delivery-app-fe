f46280c2af3862db70c2819eb4f5e00a2f265f79
Jishnu P Sreekumar <psjishnu13@gmail.com>
2020-09-13 16:25:08 +0530

    added customer declation in login api

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -30,7 +30,7 @@ const Login = ({ navigation }) => {
   const handleSubmit = () => {
     if (validateEmailAddress(email)) {
       setLoading(true);
-      const userMail = "EMAIL_AUTH" + splitterString + email;
+      const userMail = "EMAIL_AUTH" + splitterString + email + "customer";
       dispatch(login({ email: userMail, password })).then((resp) => {
         if (resp) {
           const { data: res } = resp;
